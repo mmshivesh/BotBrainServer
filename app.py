9e96859df83dd93a3afba93b5b3dbf0099be78c3
Shivesh M M <mmshivesh@gmail.com>
2019-03-24 00:48:30 +0530

    local json database, error and list retrieval

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask, request, jsonify
+import json
 from datetime import datetime
 import math
 app = Flask(__name__)
 
-sample_obj = ['L','R','R','S','H']
+## Get Json objects
+
+with open("json/error.json") as error_file:
+	error = json.load(error_file)
+with open("json/database.json") as data_file:
+	lists = json.load(data_file)
+
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    return f"<h1>Welcome to the PDP Server!</h1><p>It is currently {the_time}.</p>"
 
-    return """
-    <h1>Hello PDP!</h1>
-    <p>It is currently {time}.</p>
-    """.format(time=the_time)
-
-@app.route('/getpath')
-def getpath():
-	# request.args.get("<query-param>")
-	return jsonify(sample_obj)
+@app.route('/lists')
+def returnlists():
+	list_len = len(lists)
+	try:	# as long as the id is not NoneType
+		i = int(request.args.get('id'))
+		if(i<list_len):
+			return jsonify(lists[i])
+		else:
+			return jsonify(error)
+	except:	# If there is no id
+		temp = {"no_of_lists":list_len}
+		return jsonify(temp)
 
 if __name__=='__main__':
 	app.run(debug=True, use_reloader=True)
