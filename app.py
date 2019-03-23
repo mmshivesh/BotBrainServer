ab676af530f8575236d2056cc7c027a58b5020d4
Shivesh M M <mmshivesh@gmail.com>
2019-03-23 18:48:45 +0530

    sample landing page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import firebase
+from datetime import datetime
 import math
 app = Flask(__name__)
 
@@ -11,6 +12,17 @@ config = {
 }
 
 obj = ['L','R','R','S','H']
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
 
 @app.route('/push')
 def push():
